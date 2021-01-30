a55d4a99180880a99b3c16622874144f67d84a8c
HeadHodge <7340822+HeadHodge@users.noreply.github.com>
2021-01-29 16:21:17 -0800

    save

--- modules/hubs/hub.zone.livingRoom.js
+++ modules/hubs/hub.zone.livingRoom.js
@@ -53,31 +53,23 @@ exports.tasks = {
 		//Wake Up	
 		"Left"  : [
 			{"remote/send_command": {"entity_id": "remote.broadlink_ir_hub_downstairs_remote", "device": "Insignia FireTV", "command": "On/Off"}}, //Turn TV On
-			{"cover/set_cover_position": {"entity_id": "cover.springs_window_fashions_graber_csz1_cellular_shade_level", "position": 55}}, //Partial Open Livingroom Shade
-			{"cover/set_cover_position": {"entity_id": "cover.downstairs_slider", "position": 30}}, //Partial Open Dining Room Shade
-			{"input_select/select_option": {"entity_id": "input_select.livingroom_fireplace_duration", "option": "90 Minutes"}} //Turn Fireplace On
+			{"cover/set_cover_position": {"entity_id": "cover.springs_window_fashions_graber_csz1_cellular_shade_level", "position": 45}}, //Partial Open Livingroom Shade
+			{"cover/set_cover_position": {"entity_id": "cover.downstairs_slider", "position": 40}}, //Partial Open Dining Room Shade
+			{"input_select/select_option": {"entity_id": "input_select.livingroom_fireplace_duration", "option": "90 Minutes"}}, //Turn Fireplace On
+			{"media_player/select_source": {"entity_id": "media_player.firetv_livingroom", "source": "com.att.tv"}}
 		],
 
 		//Morning	
 		"Up"  : [
-			{"remote/send_command": {"entity_id": "remote.broadlink_ir_hub_upstairs_remote", "device": "Vizio", "command": "On/Off"}},
-			{"androidtv/adb_command": {"entity_id": "media_player.firetv_masterbedroom", "command": "POWER"}},
-			{"sleep": 3},
-			{"sonos/clear_sleep_timer": {"entity_id": "media_player.master_bedroom"}},
-			{"media_player/select_source": {"entity_id": "media_player.master_bedroom", "source": "TV"}},
-			{"sonos/join": {"entity_id": "media_player.bathroom", "master": "media_player.master_bedroom"}},
-			{"media_player/volume_set": {"entity_id": "media_player.bathroom", "volume_level": 0.45}},
-			{"media_player/volume_set": {"entity_id": "media_player.master_bedroom", "volume_level": 0.4}}
+			{"remote/send_command": {"entity_id": "remote.broadlink_ir_hub_downstairs_remote", "device": "Insignia FireTV", "command": "On/Off"}}, //Turn TV On
+			{"cover/set_cover_position": {"entity_id": "cover.springs_window_fashions_graber_csz1_cellular_shade_level", "position": 45}}, //Partial Open Livingroom Shade
+			{"cover/set_cover_position": {"entity_id": "cover.downstairs_slider", "position": 40}}, //Partial Open Dining Room Shade
+			{"input_select/select_option": {"entity_id": "input_select.livingroom_fireplace_duration", "option": "90 Minutes"}}, //Turn Fireplace On
+			{"media_player/select_source": {"entity_id": "media_player.firetv_livingroom", "source": "com.att.tv"}}
 		],
 	
 		//Daytime	
 		"Right"  : [
-			{"sonos/unjoin": {"entity_id": "media_player.bathroom"}},
-			{"androidtv/adb_command": {"entity_id": "media_player.firetv_masterbedroom", "command": "SLEEP"}},
-			{"remote/send_command" : {"entity_id": "remote.broadlink_ir_hub_upstairs_remote", "device": "Vizio", "command": "On/Off"}},
-			{"sleep": 5},
-			{"media_player/select_source": {"entity_id": "media_player.master_bedroom", "source": "Blues"}},
-			{"media_player/volume_set": {"entity_id": "media_player.master_bedroom", "volume_level": 0.3}}
 		],
 	
 		//Night	
