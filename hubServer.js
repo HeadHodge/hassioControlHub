42b000e7f73f7ea003017fd93b7f92633320212c
HeadHodge <7340822+HeadHodge@users.noreply.github.com>
2021-01-31 11:16:25 -0800

    save

--- hubServer.js
+++ hubServer.js
@@ -1,14 +1,25 @@
 var fs = require('fs');
 var http = require('http');
+var host, path;
 
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
+
+	if(req.url == '/webRemote') 
+		{host = __dirname + req.url; path = host+'/homePage.htm';}
+	else if(req.url == '/webRemote/') 
+		{host = __dirname; path = host+'/webRemote/homePage.htm';}
+	else
+		path = host + req.url;	
+	
+	fs.readFile(path, function (err,data) {
+		if (err) {
+			res.writeHead(404);
+			res.end(`Abort: Invalid Path Specified.\nUrl: ${req.url}, Host: ${host}, Path: ${path}\nError: ${JSON.stringify(err)}`);
+			return;
+		};
+		
+		res.writeHead(200);
+		res.end(data);
   });
+  
 }).listen(80);
