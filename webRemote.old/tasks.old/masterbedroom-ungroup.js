ddae0258b7fa0c4bd2549a39e8199a6269409142
HeadHodge <7340822+HeadHodge@users.noreply.github.com>
2021-02-01 20:46:39 -0800

    save

--- webRemote.old/tasks.old/masterbedroom-ungroup.js
+++
@@ -1,4 +0,0 @@
-//Group Bedroom+Bathroom Speakers
-ipRemote.case.postCommand({"reference": "masterbedroom-wake", "action": "runSequence", "sequence": [
-	{"sonos/unjoin": {"entity_id": "media_player.bathroom"}}
-]});
