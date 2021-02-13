76cd39a2df0116cef5590f1ea62260a283a0355b
HeadHodge <7340822+HeadHodge@users.noreply.github.com>
2021-02-13 15:28:55 -0800

    Update wsOutput.js

--- core/wsOutput.js
+++ core/wsOutput.js
@@ -4,7 +4,7 @@
 const debug = require('/inputHub/core/debugLog.js').debug;
 const webSocket = require('/root/node_modules/ws');
 
-var connection = null, isConnected = null, messageId = 1;
+var connection = null, isConnected = null, activeSessions = {}, sessionId = 1;
 
 //##########################################
 onOpen =  function() {
@@ -25,15 +25,71 @@ console.log('Enter onClose');
 onMessage = function(message) {
 //##########################################
 var payload = JSON.parse(message);
-debug.log(`Enter onMessage, Received server message: `, message);
+
+	debug.log(`Enter onMessage, Received server message: `, message);
 
 	if(payload.type == 'auth_required') return connection.send('{"type": "auth", "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1YmM0ZGYxNGY4ZGE0MTdkYTNhZjdkNjkwYzg0NDQ2ZSIsImlhdCI6MTYxMzAxMDQ4MiwiZXhwIjoxOTI4MzcwNDgyfQ.MffxNYX4VssITLgdZBPilKTq3p4R9RuoQP2yeeoyyPw"}');
 	if(payload.type == 'auth_ok') return isConnected = true;
 	if(payload.type != 'result') return console.log(`Unknown Message from wsOutput: `, message);
-	if(payload.success) return console.log(`Server result: messageId: ${payload.id}, status: ${payload.success}`);
 
+	console.log(` \n`);
+	console.log(`==================================================================================================`);
+
+	if(activeSessions[payload.id] && !payload.success) {
+	//Report Session Error
+		console.log(`Send error reply, sessionId: ${payload.id}, result: `, payload.result, `\ntask: `, activeSessions[payload.id].task);
+		activeSessions[payload.id].onReply("***TASK FAILED***\n", message);
+	};		
+
+	delete activeSessions[payload.id]; //Delete completed session
 	console.log(`Server result: messageId: ${payload.id}, status: ${payload.success}, result: `, payload.result);
-	connection.reply(message);
+};
+
+//##########################################
+runTask = function(input, reply) {
+//##########################################
+console.log(`Enter runTask`);
+var task = JSON.parse(input);
+var service, key, command, payload={};
+
+	if(!isConnected) return console.log(`Abort runTask: Not connected to server`);
+	if(task.length == 0) return console.log(`runTask Completed`);
+
+	service = task[0];
+	task.shift();
+	key = Object.keys(service)[0];
+	command = key.split('/');
+
+	if(command.length == 1) {
+		var duration = 0;
+		if(command[0] == 'sleep') duration = service[key] * 1000;
+
+		console.log(`Sleep: ${duration} millisecs`);
+		setTimeout(function timeout() {
+			runTask(JSON.stringify(task), reply);
+		}, duration);
+		
+		return;
+	};
+
+	payload = {
+		"id": sessionId, 
+		"type": "call_service",	
+		"domain": command[0],
+		"service": command[1],
+		"service_data": service[key]
+	};
+	
+	activeSessions[sessionId] = {
+		"task": payload,
+		"onReply": reply
+	};
+	
+	++sessionId;
+		
+	console.log(`Call service, id: ${payload.id}, domain: ${payload.domain}, service: ${payload.service}`);
+	connection.send(JSON.stringify(payload));
+	return runTask(JSON.stringify(task), reply);
 };
 
 //##########################################
@@ -50,45 +106,6 @@ console.log(`Enter openConnection`);
 	connection.on('message', onMessage);
 };
 
-//##########################################
-runTask = function(input, reply) {
-//##########################################
-console.log(`Enter runTask`);
-var task = JSON.parse(input);
-var service, key, command, payload={"id": messageId, "type": "call_service"};
-
-	if(!isConnected) return console.log(`Abort runTask: Not connected to server`);
-	if(task.length == 0) return console.log(`runTask Completed`);
-
-	service = task[0];
-	task.shift();
-	key = Object.keys(service)[0];
-	command = key.split('/');
-
-	if(command.length == 1) {
-		var duration = 0;
-		if(command[0] == 'sleep') duration = service[key] * 1000;
-		console.log(`Sleep: ${duration} millisecs`);
-
-		setTimeout(function timeout() {
-			runTask(JSON.stringify(task), reply);
-		}, duration);
-		
-		return;
-	};
-	
-	payload.domain = command[0];
-	payload.service = command[1];
-	payload.service_data = service[key];
-	console.log(`Send payload, domain: ${payload.domain}, service: ${payload.service}`);
-	
-	++messageId;
-	connection.reply = reply;
-	connection.send(JSON.stringify(payload));
-		
-	return runTask(JSON.stringify(task), reply);
-};
-
 ////////////////////////////////////////////
 //                MAIN
 //Open connection with Hub and send request
@@ -96,5 +113,5 @@ var service, key, command, payload={"id": messageId, "type": "call_service"};
 console.log('Loaded wsOutput.js');
 
 	global.onOutput = runTask;
-	exports.runTask = runTask;
+	//exports.runTask = runTask;
 	openConnection();	
