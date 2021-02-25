9f748830cd66e401b79eba7d4faa691be497a21c
HeadHodge <7340822+HeadHodge@users.noreply.github.com>
2021-02-24 23:51:34 -0800

    save

--- btNode/classes/ip2bt/ip2btBridge.py
+++ btNode/classes/ip2bt/ip2btBridge.py
@@ -8,30 +8,45 @@ import sys, time, json
 #from gi.repository import GLib
 #from dbus.mainloop.glib import DBusGMainLoop
 from wsServer import wsServer
-#from btServer import btServer
+from btServer import btServer
 
-def pName(myname):
-    print('name:', myname)
+class ip2btBridge:
+    print('Load ip2btBridge')
+    """
+    """  
+    def __init__(self):
+        print('Starting ip2btBridge')
+        threading.Thread(target=self.def1()).start()
 
-def def1():
-    print('def1')
-    server = wsServer(pName)
+        #loop = asyncio.get_event_loop()
+        #loop.create_task(self.def1())
+
+        print('End Test')
+        asyncio.get_event_loop().run_forever()
+        
+    def myName(self, name):
+        print(f'my name is: {name}')
+        
+    def def1(self):
+        print('def1')
+        server = wsServer(self)
+
+    async def def2(self):
+        print('def2')
+        server = btServer()
+        
+    """
     #asyncio.set_event_loop(asyncio.new_event_loop())        
     #asyncio.get_event_loop().run_forever()
+    print('Start Test')
+    #asyncio.get_event_loop().run_until_complete(def1())
+    #asyncio.get_event_loop().run_until_complete(wsServer)
+    #asyncio.run(def1())
+    #asyncio.get_event_loop().run_forever()
 
-async def def2():
-    print('def2')
-    server = btServer()
-
-
-print('Start Test')
-#asyncio.get_event_loop().run_until_complete(def1())
-#asyncio.get_event_loop().run_until_complete(wsServer)
-#asyncio.run(def1())
-#asyncio.get_event_loop().run_forever()
-
-x = threading.Thread(target=def1())
-x.start()
-##asyncio.get_event_loop().run_until_complete(def1())
-print('End Test')
-asyncio.get_event_loop().run_forever()
+    threading.Thread(target=def1()).start()
+    ##asyncio.get_event_loop().run_until_complete(def1())
+    print('End Test')
+    asyncio.get_event_loop().run_forever()
+    """
+myclass = ip2btBridge()
--- btNode/classes/ip2bt/wsServer.py
+++ btNode/classes/ip2bt/wsServer.py
@@ -8,27 +8,23 @@ class wsServer:
     """
     """
     
-    def __init__(self, pname):
-        print('Starting wsServer')
-        pname('pissy')
-        #self._ip = ip
-        #self._port = port
-        start_server = websockets.serve(self.onConnect, "127.0.0.1", 8181)
+    def __init__(self, bridge=None, options={"ip":"127.0.0.1", "port":8181}):
+        print('Starting wsServer', options)
+        
+        bridge.myName('Bobby')
+        
+        start_server = websockets.serve(self.onConnect, options['ip'], options['port'])
         print('Server Created')
-        #eventloop = asyncio.new_event_loop()
         eventloop = asyncio.get_event_loop()
-        #self.asyncio.set_event_loop(eventloop)
         eventloop.run_until_complete(start_server)
-        #eventloop.run_forever()
 
     async def onConnect(self, websocket, path):
-        #global myName         
-        print('Connected', myName)
+        print('Connected')
 
         try:
             await websocket.send('{"format": "greeting", "greeting": "Hello?", "from": ""}')
             async for text in websocket:
-                #print('received text: ', json.loads(text))
+                print('received text: ', json.loads(text))
                 await websocket.send('{"format": "reply", "reply": "Got It"}')
         except Exception as e:
             print(e)
